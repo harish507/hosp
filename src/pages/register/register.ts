228226cbf886eb42de23131b944da0050b3650cf
unknown <harishbali1997@gmail.com>
2018-09-28 20:18:03 +0530

    without modified code

---
+++ src/pages/register/register.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginPage } from '../login/login';
+
+/**
+ * Generated class for the RegisterPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage  {
+  register:FormGroup;
+  userData = { "username": "","password":""}
+  constructor(public navCtrl : NavController,public navParams: NavParams) {
+  }
+ 
+  ngOnInit() {
+    let EMAILPATTERN = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    this.register = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(4), Validators.maxLength(10)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+      name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(4), Validators.maxLength(30)]),
+      phonenumber: new FormControl('',[Validators.required,Validators.maxLength(10)]),
+      email: new FormControl('', [Validators.required, Validators.pattern(EMAILPATTERN)]),
+
+      
+    });
+    
+  }
+ 
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+  login(){
+    this.navCtrl.push(LoginPage);
+  }
+
+}
